c044d0f81c7524adda4e76dcfcddb92f096c44a1
Moustafa Safwat ElSayed <mostafa.safwat.1@gmail.com>
2017-04-19 16:23:56 +0200

    Change Class name from cacherepository to redisrepository

--- DataAccessLayer/Repositories/RedisRepository.cs
+++ DataAccessLayer/Repositories/RedisRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    internal class FileRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    internal class RedisRepository<TEntity> : IRepository<TEntity> where TEntity : class
     {
         public TEntity Insert(TEntity entity)
         {
@@ -19,6 +19,7 @@ namespace DataAccessLayer.Repositories
             throw new NotImplementedException();
         }
 
+
         public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
         {
             throw new NotImplementedException();
@@ -29,10 +30,12 @@ namespace DataAccessLayer.Repositories
             throw new NotImplementedException();
         }
         
+
         public void Update(TEntity entity)
         {
             throw new NotImplementedException();
         }
+
 
         public void Delete(TEntity entity)
         {
